2cf05b820d480734d0f018abfe778aa5563d24ff
Hashith Sithuruwan <mailtohashith@gmail.com>
2024-05-06 10:51:44 +0530

    added

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const cors = require("cors");
 app.use(express.json());
 
-const url = "http://hashith.online:8080";
+const url = "http://hashith.online";
 const corsOption = {
   origin: url,
   credentials: true,
